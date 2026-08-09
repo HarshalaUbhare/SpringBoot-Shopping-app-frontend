b9bfad067752885dbd90d771b325f3f2a6fae34f
Harshala-Ubhare <harshalaubhare@gmail.com>
2026-08-10 01:48:41 +0530

    fix: disable Tailwind Preflight to prevent Bootstrap style conflict

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
     extend: {
       colors: {
